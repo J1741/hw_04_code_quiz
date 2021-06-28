0c2e738bdceaa47fe34e4a9d8d0c53c58ae86ee9
J1741 <84889065+J1741@users.noreply.github.com>
2021-06-28 19:02:05 -0400

    adding bootstrap responsive layout and styling to start screen; preventing default on quiz start button

--- js/index_script.js
+++ js/index_script.js
@@ -11,20 +11,53 @@
 
 // access timer element by id
 var timeEl = document.querySelector("#time");
+console.log(timeEl);
 
 // set timer starting point
 var secondsLeft = 5;
+console.log(secondsLeft);
 
 // access start button by id
 var quizStartBtn = document.querySelector("#quiz-start-btn");
+
+console.log(quizStartBtn);
+
+// TESTING: add test questions
+var questions = [
+
+  {
+    title: "question1",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit:",
+    choices: ["test1", "test2", "test3", "test4"],
+    answer: "test1" 
+  },
+  {
+    title: "question2",
+    text: "Aenean vel iaculis ante, eu interdum mauris:",
+    choices: ["test5", "test6", "test7", "test8"],
+    answer: "test5"
+  },
+  {
+    title: "question3",
+    text: "Sed id erat et augue gravida rutrum:",
+    choices: ["test9", "test10", "test11", "test12"],
+    answer: "test9" 
+  }
+];
+
+// log questions
+console.log(questions);
 
 /******************/
 /* 2.0 Quiz Timer */
 /******************/
 
+
 // Listen for click on quiz start button
-quizStartBtn.addEventListener("click", 
-function setTime() {
+function setTime(event) {
+  // prevent default
+  event.preventDefault();
+  console.log(event);
   // Sets interval in variable 
   var timerInterval = setInterval(function() {
 
@@ -42,5 +75,7 @@ function setTime() {
     }
 
   }, 1000);
-  
-});
+
+};
+
+quizStartBtn.addEventListener("click", setTime);
